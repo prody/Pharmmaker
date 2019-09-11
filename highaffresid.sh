55664700dea1f39bba119da7affb19728646fabd
James Krieger <jamesmkrieger@gmail.com>
2019-09-11 17:06:35 -0400

    updated steps 1 and 2

--- highaffresid.sh
+++ highaffresid.sh
@@ -1,42 +1,7 @@
-# 2019-06-30 Bahar LAB, University of Pittsburgh  bahar@pitt.edu
-# Written by Ji Young Lee
-# Calculate high affinity residues with trajectory of 
-# Druggability molecular dynamics simulations
-
-######### Check here  #######
-# PDB file name and path
-PDB='..\/drugui-simulation\/protein-probe.pdb'
-# DCD file name and path
-DCD='..\/drugui-simulation\/protein-probe.dcd'
-# protein chain ID in pdb
-CHAIN=' A B '    
-# first residue number of protein for analysis
-FIRSTRESID=5
-# last residue number of protein for analysis
-LASTRESID=261    # 261
-# probe molecule ID in pdb
-PROBE=' IPRO ACAM ACTT IPAM IBTN IMID ' 
-# cutoff between residue and probe      
-CUTOFF=4.0
-# frequency of dcd for analysis
-STEP=1
-######### Check here  #######
-
-for FCHAIN in $CHAIN
-do
-for FPROBE in $PROBE
-do
-for (( FF = $FIRSTRESID ; FF <= $LASTRESID  ; FF++ ))
-do
-sed -e "s/APDB/$PDB/g" -e "s/ADCD/$DCD/g" -e "s/ACHAIN/$FCHAIN/g" -e "s/ARESID/$FF/g" -e "s/APROBE/$FPROBE/g" -e "s/ACUTOFF/$CUTOFF/g" -e "s/ASTEP/$STEP/g" TCL/highaffresid.tcl > __highaffresid.tcl
-vmd -dispdev text -e __highaffresid.tcl
-cat _out-$FCHAIN-$FPROBE.dat        >> out-$FCHAIN-$FPROBE.dat
-cat _out-detail-$FCHAIN-$FPROBE.dat >> out-detail-$FCHAIN-$FPROBE.dat
-done
-done
+argString=""
+i=0
+for (( i = 1  ; i <= $# ; i++ )); do
+  argString+="${!i} "
 done
 
-rm _*
-mkdir highaffresid
-mv out-* highaffresid
-
+env VMDARGS='text with blanks' vmd -dispdev text -e $PHARMMAKER_HOME/highaffresid.tcl -args $argString
